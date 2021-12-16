01c444ee6b3457a398ef4c4e1920b180fc72d8c2
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-12-16 20:02:08 +0530

    feat: deploy configurations

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create( {baseURL: 'http://localhost:5000'})
+const API = axios.create( {baseURL: 'https://social-memories-app.herokuapp.com'})
 
 //function that runs on each one of request
 API.interceptors.request.use( (req) => {
